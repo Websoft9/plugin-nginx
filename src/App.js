dca14d7ea544eb25bf94153fca12327ae9820c19
zhaojing1987 <jing.zhao@websoft9.com>
2023-07-14 16:26:05 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,57 +1,102 @@
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import cockpit from 'cockpit';
 import { useEffect, useState } from 'react';
+import { Alert } from 'react-bootstrap';
 import Spinner from 'react-bootstrap/Spinner';
 import './App.css';
 
 function App() {
-  const [nginxproxymanagerTokens, setNginxproxymanagerTokens] = useState(null);
-  const [nginxproxymanagerHomePage, setNginxproxymanagerHomePage] = useState(null);
+  const [iframeSrc, setIframeSrc] = useState(null);
+  const [iframeKey, setIframeKey] = useState(Math.random());
+  const [showAlert, setShowAlert] = useState(false);
+  const [alertMessage, setAlertMessage] = useState("");
+
+  var baseURL;
 
   const getData = async () => {
-    try {
-      const response = await axios.get('../myapps/config.json'); //从项目下读取配置文件
-      if (response.status === 200) {
-        const config = response.data.NGINXPROXYMANAGER;
-        const { NGINXPROXYMANAGER_USERNAME, NGINXPROXYMANAGER_PASSWORD, NGINXPROXYMANAGER_AUTH_URL, NGINXPROXYMANAGER_HOME_PAGE, NGINXPROXYMANAGER_NIKENAME } = config;
-        setNginxproxymanagerHomePage(NGINXPROXYMANAGER_HOME_PAGE);
+    let protocol = window.location.protocol;
+    let host = window.location.host;
+    let tokens = localStorage.getItem("nginx_tokens");
+    let nikeName = localStorage.getItem("nginx_nikeName");
+    baseURL = protocol + "//" + (/^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}/.test(host) ? host.split(":")[0] : host);
 
-        //调用nginxproxymanager的登录API，模拟登录
-        const authResponse = await axios.post(NGINXPROXYMANAGER_AUTH_URL, {
-          identity: NGINXPROXYMANAGER_USERNAME,
-          secret: NGINXPROXYMANAGER_PASSWORD
-        });
-        if (authResponse.status === 200) {
-          const tokens = authResponse.data.token;
-          setNginxproxymanagerTokens(tokens);
-          const nginx_proxy_manager_tokens = [{ "t": tokens, "n": NGINXPROXYMANAGER_NIKENAME }];
-          window.localStorage.setItem('nginx-proxy-manager-tokens', JSON.stringify(nginx_proxy_manager_tokens)); //关键是将通过API登录后获取的tokens，存储到本地localStorage
-        } else {
-          console.log('Error:', authResponse);
+    if (!tokens || !nikeName) {
+      let data = await cockpit.spawn(["docker", "inspect", "-f", "{{range.NetworkSettings.Networks}}{{.IPAddress}}{{end}}", "websoft9-appmanage"]);
+      let IP = data.trim();
+      if (IP) {
+        let response = await cockpit.http({ "address": IP, "port": 5000 }).get("/AppSearchUsers", { "plugin_name": "nginx" });
+        response = JSON.parse(response);
+        if (response.ResponseData) {
+          var userName = response.ResponseData.user?.user_name;
+          var userPwd = response.ResponseData.user?.password;
+          nikeName = response.ResponseData.user?.nick_name;
+
+          const authResponse = await axios.post(baseURL + "/nginxproxymanager/api/tokens", {
+            identity: userName,
+            secret: userPwd
+          });
+          if (authResponse.status === 200) {
+            tokens = authResponse.data.token;
+            window.localStorage.setItem("nginx_tokens", tokens);
+            window.localStorage.setItem("nginx_nikeName", nikeName);
+          } else {
+            setShowAlert(true);
+            setAlertMessage("Auth Nginxproxymanager Error.")
+          }
         }
       }
-      else {
-        console.log('Error:', response);
+    }
+
+    setIframeKey(Math.random());
+    var newHash = window.location.hash;
+    if (newHash.includes("/nginxproxymanager")) {
+      var index = newHash.indexOf("#");
+      if (index > -1) {
+        var content = newHash.slice(index + 1);
+        setIframeKey(Math.random());
+        setIframeSrc(baseURL + content + "?Token=" + tokens + "&Name=" + nikeName);
       }
-    } catch (error) {
-      console.log('Error:', error);
+    }
+    else {
+      setIframeSrc(baseURL + "/nginxproxymanager/?Token=" + tokens + "&Name=" + nikeName);
     }
   }
 
-  useEffect(() => {
-    getData();
+  const handleHashChange = () => {
+    var newHash = window.location.hash;
+    if (newHash.includes("/nginxproxymanager")) {
+      var index = newHash.indexOf("#");
+      if (index > -1) {
+        var content = newHash.slice(index + 1);
+        setIframeKey(Math.random());
+        setIframeSrc(baseURL + content);
+      }
+    }
+  }
+
+  useEffect(async () => {
+    await getData();
+
+    window.addEventListener("hashchange", handleHashChange, true);
+    return () => {
+      window.removeEventListener("hashchange", handleHashChange, true);
+    };
   }, []);
 
   return (
     <>
       {
-        nginxproxymanagerTokens && nginxproxymanagerHomePage ? (
+        (iframeKey && iframeSrc) ? (
           <div className='myNginx' key='container'>
-            <iframe title='nginxproxymanager' src={nginxproxymanagerHomePage} />
+            <iframe key={iframeKey} title='nginxproxymanager' src={iframeSrc} />
           </div>
         ) : (
           <div className="d-flex align-items-center justify-content-center m-5">
             <Spinner animation="border" variant="secondary" />
+            {showAlert && <Alert variant="danger" className="my-2">
+              {alertMessage}
+            </Alert>}
           </div>
         )
       }
